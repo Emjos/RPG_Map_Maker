d931b922239f9d0b65144fac8e4bc9f8455383e7
Emjos <hubert.lyczko@gmail.com>
2020-02-13 10:46:32 +0000

    clean

--- src/sample/controllers/old/ToDo.java
+++ src/sample/controllers/old/ToDo.java
@@ -5,15 +5,15 @@ public class ToDo {
 
 
     // wgrac itemy i akcje okienka i listy
-    // dodac pointy
 
 
-    //------ obiekty po resize wczytuja ostatni background, trzeba to zmienic
+
+
     //------ sprowowac dodac tablice stringow do kazdej mapy, dodanie background url do pointu [x][y}
     // sprobowac w klasie Tile dodac urlBackground, wraz ze flaga bylby nadpisany a bez flagi dawal by domyslnie ta sama grafike
 
 // 12/02 zmienic kafelki jezeli cos na nich jest
-    //// 08.02  Nazwy w Mostach i Itemach nie powinny sie powtarzac !!
+
 
     /*
     Kafelki do terra powinny byc mniejsze , napewno drogi
